9684d7c39656d9e500f9431f13f7af0c54f20746
Mihail Pupynin <misha-pupynin@mail.ru>
2021-09-20 18:14:32 +0700

    init board

--- src/App.js
+++ src/App.js
@@ -4,11 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>PACMAN!</h1>
-      </header>
+      <Game />
     </div>
   );
 }
 
+function Game(){
+ 
+  function createLayout(num) {
+    const layout =[];
+    for(let i=0; i < num; i++){
+      layout.push(0);
+      
+    }
+    return layout;
+  }
+  const layout = createLayout(784);
+
+  function createBoard() {
+    const squares =[]
+    for(let i=0; i < layout.length; i++){
+      if(layout[i] === 0) {
+        squares.push(<div className="pac-dot" key={i}></div>)
+      } else if (layout[i] === 1) {
+        squares.push(<div className="wall" key={i}></div>)
+      } else if (layout[i] === 3) {
+        squares.push(<div className="power-pellet" key={i}></div>)
+      }
+    }
+    return squares;
+  }
+  const board = createBoard();
+  return (
+    <div className="grid">{board}</div>
+  );
+}
 export default App;
